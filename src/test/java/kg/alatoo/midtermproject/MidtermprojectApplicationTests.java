beb267d97a4b477aec611ae730a641a6d3f33c8e
Nurgul <adisovanur7@gmail.com>
2024-03-18 12:23:16 +0600

    initial commit

---
+++ src/test/java/kg/alatoo/midtermproject/MidtermprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.alatoo.midtermproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
